3afc104810339e3d7968a759b1af2d4edb770069
Pitambar Jha <nishjha4u@gmail.com>
2019-08-02 18:49:10 +0545

    travel exp added to msc

--- Ems.Database/dbo/Tables/MiscExpenses.sql
+++ Ems.Database/dbo/Tables/MiscExpenses.sql
@@ -7,6 +7,15 @@
     [Comment]      NVARCHAR (MAX) NULL,
     [Price]        FLOAT (53)     NOT NULL,
     [Date]         DATETIME2 (7)  NOT NULL,
-    CONSTRAINT [PK_MiscExpenses] PRIMARY KEY CLUSTERED ([ID] ASC)
+    [TraveExpID]   INT            DEFAULT ((0)) NOT NULL,
+    CONSTRAINT [PK_MiscExpenses] PRIMARY KEY CLUSTERED ([ID] ASC),
+    CONSTRAINT [FK_MiscExpenses_TravelExpenses_TraveExpID] FOREIGN KEY ([TraveExpID]) REFERENCES [dbo].[TravelExpenses] ([ID])
 );
+
+
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_MiscExpenses_TraveExpID]
+    ON [dbo].[MiscExpenses]([TraveExpID] ASC);
 
